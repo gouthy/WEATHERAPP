d7ba41fa4951424f2ead38cb614709fd79822777
Goutam Konapala <goutam.iitbbs@gmail.com>
2023-08-20 21:30:16 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <h1 className="flex items-center min-w-[720px] min-h-screen bg-blue-500">
+  
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
